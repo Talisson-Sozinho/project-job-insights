c083d669e61a5f47b528e93b02bc48db38c10313
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-04-19 01:28:29 -0300

    feito requisito 03 - função "get_unique_industries"

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,22 +1,11 @@
 from typing import List, Dict
+from jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
-    """Checks all different industries and returns a list of them
-
-    Must call `read`
-
-    Parameters
-    ----------
-    path : str
-        Must be passed to `read`
-
-    Returns
-    -------
-    list
-        List of unique industries
-    """
-    raise NotImplementedError
+    jobs_data = read(path)
+    set_of_industries = {linha["industry"] for linha in jobs_data}
+    return [jobs_industry for jobs_industry in set_of_industries]
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
