7226b18171cb301971e7ce5da9cfd30a87cc5b09
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-04-19 01:42:57 -0300

    fix requisito 03 - função "get_unique_industries"

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,11 +1,11 @@
 from typing import List, Dict
-from jobs import read
+from src.insights.jobs import read
 
 
 def get_unique_industries(path: str) -> List[str]:
     jobs_data = read(path)
     set_of_industries = {linha["industry"] for linha in jobs_data}
-    return [jobs_industry for jobs_industry in set_of_industries]
+    return [industry for industry in set_of_industries if industry != '']
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
