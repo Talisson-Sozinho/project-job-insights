c1164fcddbd11516763966b8d935018457e86caf
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-04-19 03:02:19 -0300

    feito requisito 07 - função "filter_by_industry"

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -5,22 +5,8 @@ from src.insights.jobs import read
 def get_unique_industries(path: str) -> List[str]:
     jobs_data = read(path)
     set_of_industries = {linha["industry"] for linha in jobs_data}
-    return [industry for industry in set_of_industries if industry != '']
+    return [industry for industry in set_of_industries if industry != ""]
 
 
 def filter_by_industry(jobs: List[Dict], industry: str) -> List[Dict]:
-    """Filters a list of jobs by industry
-
-    Parameters
-    ----------
-    jobs : list
-        List of jobs to be filtered
-    industry : str
-        Industry for the list filter
-
-    Returns
-    -------
-    list
-        List of jobs with provided industry
-    """
-    raise NotImplementedError
+    return [job for job in jobs if job["industry"] == industry]
