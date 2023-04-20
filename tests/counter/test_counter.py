20d498474ec4eeeb0fb461d092eb28cf9ae44ce9
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-04-19 21:09:03 -0300

    feito requisito 10 - teste da função "count_ocurrences"

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,14 @@
-# from src.pre_built.counter import count_ocurrences
+from src.pre_built.counter import count_ocurrences
+from unittest.mock import mock_open, patch
 
 
 def test_counter():
-    pass
+    path = "data/jobs.csv"
+
+    text_base = """Texto para finalidade de testar a função
+    com muitas palavras só preciso de mais repetição nesse texto"""
+
+    with patch("builtins.open", mock_open(read_data=text_base)):
+        assert count_ocurrences(path, "Texto") == 2
+        assert count_ocurrences(path, "notworld") == 0
+        assert count_ocurrences(path, "texto") == 2
