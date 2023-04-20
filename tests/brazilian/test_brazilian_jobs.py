1fc20294bc83ecd1f88e10b3b2490b3aa9ff64d7
Talisson Sozinho <talissonlutero8080@gmail.com>
2023-04-19 21:32:43 -0300

    feito requisito 11 - teste da função "read_brazilian_file"

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,10 @@
-# from src.pre_built.brazilian_jobs import read_brazilian_file
+from src.pre_built.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    path = "tests/mocks/brazilians_jobs.csv"
+    dicts_traduzido = read_brazilian_file(path)
+    for dict_traduzido in dicts_traduzido:
+        assert 'title' in dict_traduzido
+        assert 'salary' in dict_traduzido
+        assert 'type' in dict_traduzido
